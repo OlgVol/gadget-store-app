5c16cb343f3309fa7d10cfa45312d8f74488b804
Volkova <olga.volkova@accenture.com>
2023-03-24 16:58:59 +0200

    added component, added element

---
+++ src/app/feature-section/feature-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-section',
+  templateUrl: './feature-section.component.html',
+  styleUrls: ['./feature-section.component.scss']
+})
+export class FeatureSectionComponent {
+  title = 'Why Choose us?'
+}
