df28ad46c5033f6fa9567939c2f907e1815eadc9
Volkova <olga.volkova@accenture.com>
2023-03-24 10:41:01 +0200

    working on style hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { MaterialModule } from './shared/material.module';
 import { HeaderComponent } from './shared/ui-material/header/header.component';
 import { FooterComponent } from './shared/ui-material/footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomePageComponent
+    HomePageComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -4,6 +4,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 
 
@@ -18,7 +20,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatSlideToggleModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule
   ]
 })
 export class MaterialModule { }
