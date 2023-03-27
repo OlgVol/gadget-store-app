47237074347d224c6ca318a99417f7c913e2feb5
Volkova <olga.volkova@accenture.com>
2023-03-27 18:15:15 +0300

    added component product card

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -9,10 +9,58 @@ export class ProductCardComponent {
   product = [
     {
       title: 'Smartwatch',
-      price: '199.99',
-      imgUrl: '../../assets/images/smartwatch.png',
-      description:
-        'This smartwatch offers a range of features including fitness tracking, notifications, and voice commands. Its long-lasting battery and durable design make it perfect for everyday wear.'
-    }
+
+      price: 199.99,
+
+      short_description:
+        ' Stay connected on the go with this sleek and versatile smartwatch.',
+    },
+    {
+      title: 'Smart Assistant',
+
+      price: 149.99,
+
+      short_description:
+        ' Make your life easier with this smart assistant that can answer your questions.',
+    },
+    {
+      title: 'Smartphone',
+
+      price: 699.99,
+
+      short_description:
+        ' Stay connected and productive on the go with this powerful smartphone.   ',
+    },
+    {
+      title: 'Earbuds',
+
+      price: 129.99,
+
+      short_description:
+        ' Enjoy your favorite music and podcasts with these comfortable earbuds.',
+    },
+    {
+      title: 'Tablet',
+
+      price: 399.99,
+
+      short_description:
+        ' Stay productive and entertained on the go with this versatile tablet.',
+    },
+    {
+      title: 'Portable Speaker',
+
+      price: 99.99,
+
+      short_description:
+        ' Take your music anywhere with this compact and powerful portable speaker.',
+    },
+    {
+      title: 'Web Camera',
+
+      price: 59.99,
+
+      short_description: ' Stay connected with this high-quality web camera. ',
+    },
   ];
 }
