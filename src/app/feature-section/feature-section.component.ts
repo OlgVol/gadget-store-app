f5fba7be7ebfae1bfb9efba06daff5719ddee8c9
Volkova <olga.volkova@accenture.com>
2023-03-27 12:05:47 +0300

    replaced cards to ngFor loop

--- src/app/feature-section/feature-section.component.ts
+++ src/app/feature-section/feature-section.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class FeatureSectionComponent {
   title = 'Why Choose us?'
+
+  cards = [{
+    title: 'Wireless Freedom',
+    content: 'wireless gadget that provide freedom of movement while using them',
+    imgUrl: '../../assets/images/wifi.png'
+  },
+  {
+    title: 'Stay Connect',
+    content: 'gadget that help people stay connected with their loved ones and colleagues',
+    imgUrl: '../../assets/images/smart-device.png'
+  },
+  {
+    title: 'Smart Home',
+    content: 'gadget that make your home smarter and more efficient at the space of your own home',
+    imgUrl: '../../assets/images/smart-home.png'
+  }
+]
+
 }
