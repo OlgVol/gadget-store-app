0273afe5d8e7e3c5939af0d25029889a03b68e9d
Volkova <olga.volkova@accenture.com>
2023-03-28 15:18:19 +0300

    added handleError

--- src/app/shared/product-service/product.service.ts
+++ src/app/shared/product-service/product.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { IProduct } from 'src/app/products/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   public productUrl = 'http://localhost:3000/products';
@@ -12,9 +12,18 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(this.productUrl)
-    .pipe(
-      tap(data => console.log('All: ',JSON.stringify(data)))
-      );
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap((data) => console.log('All: ', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `Errro: ${err.error.message}`;
+    } else {
+      errorMessage = `Errored code: ${err.status}, error is: ${err.message}`;
+    }
+    return throwError(() => errorMessage);
   }
 }
