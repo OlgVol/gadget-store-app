8c968aed03e7a09bf2616e786bc7f4105f47955e
Volkova <olga.volkova@accenture.com>
2023-04-07 15:29:35 +0300

    adde image default, cleaned data base

--- src/app/add-new-product-dialog/add-new-product-dialog.component.ts
+++ src/app/add-new-product-dialog/add-new-product-dialog.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IProduct } from '../models/product.model';
 import { ProductService } from '../shared/product-service/product.service';
 import { Subscription } from 'rxjs';
-import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SuccessToasterComponent } from '../success-toaster/success-toaster.component';
 
@@ -11,34 +10,34 @@ import { SuccessToasterComponent } from '../success-toaster/success-toaster.comp
   templateUrl: './add-new-product-dialog.component.html',
   styleUrls: ['./add-new-product-dialog.component.scss'],
 })
-
 export class AddNewProductDialogComponent {
-product: IProduct = {
-  title: "",
-  short_description: "",
-  long_description : "",
-  price: "",
-  warranty_period: "",
-  RAM: "",
-}
-sub!: Subscription;
-submitted = false;
-durationInSeconds = 5;
+  product: IProduct = {
+    title: '',
+    short_description: '',
+    long_description: '',
+    price: '',
+    warranty_period: '',
+    RAM: '',
+    imgUrl : "./assets/images/tablet.png"
+  };
+  sub!: Subscription;
+  submitted = false;
+  durationInSeconds = 5;
 
-constructor(private service: ProductService,
-  private _snackBar: MatSnackBar
-   ) {}
+  constructor(
+    private service: ProductService,
+    private _snackBar: MatSnackBar
+  ) {}
 
-onSubmit(){
-  this.submitted =true
-  this._snackBar.openFromComponent(SuccessToasterComponent,{
-    duration:this.durationInSeconds * 1000,
-    panelClass: ['green-snackbar']
-  })
- this.service.addProduct(this.product)
- .subscribe(
-  data => console.log('succses', data),
-  error => console.log('Error!', error)
- )
-}
+  onSubmit() {
+    this.submitted = true;
+    this._snackBar.openFromComponent(SuccessToasterComponent, {
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['green-snackbar'],
+    });
+    this.service.addProduct(this.product).subscribe(
+      (data) => console.log('succses', data),
+      (error) => console.log('Error!', error)
+    );
+  }
 }
